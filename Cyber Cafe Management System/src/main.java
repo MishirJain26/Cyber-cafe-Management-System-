c3da6cd883e49c734bfadb625df6ba341ed9dfed
22ndPilot-Avzeet <Abhijeet20072007@gmail.com>
2026-01-05 21:39:44 +0530

    Just experimenting

--- Cyber Cafe Management System/src/main.java
+++ Cyber Cafe Management System/src/main.java
@@ -3,5 +3,6 @@ public class main
     public static void main(String[] args)
     {
         System.out.println("Cyber Cafe Management System Running!");
+        System.out.println("Hi Mishir");
     }
 }
