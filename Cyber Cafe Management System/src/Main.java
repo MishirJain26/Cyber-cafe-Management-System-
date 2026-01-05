56ff82ba7ca20d45d2ef4dda872b29592b2f3b5f
22ndPilot-Avzeet <Abhijeet20072007@gmail.com>
2026-01-05 22:20:36 +0530

    Ho gaya

--- Cyber Cafe Management System/src/Main.java
+++ Cyber Cafe Management System/src/Main.java
@@ -1,9 +1,10 @@
-public class main
+class group
 {
     public static void main(String[] args)
     {
         System.out.println("Cyber Cafe Management System Running!");
         System.out.println("Hi Mishir");
         System.out.println("Bhai ho jaana yaar jaldi se");
+        System.out.println("Yayyyy!!! Ho gaya!!!!");
     }
 }
